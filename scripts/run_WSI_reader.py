6a1139d9bf07af98be482edd94db1e2f3d9588ae
Steve Hamel <73612625+hamelst@users.noreply.github.com>
2021-10-14 15:40:52 -0400

    added my WSI_reader module with 'scripts/run_WSI_reader.py' as an example of how to use it.

--- scripts/run_WSI_reader.py
+++ scripts/run_WSI_reader.py
@@ -12,4 +12,27 @@ sys.path.append(os.path.join(working_directory,'pkg'))
 img_object  =  WSI_reader.WSI_reader(img_path)
 svs_np_array = img_object.pull_np((5000,5000),3000,200)
 
+"""WSI_reader.WSI_reader(img_path).pull_np(coords, dimensions, output_size)
+
+        Synopsis
+        ----------
+        Creates an NP array with a subsection–or the whole image—of a WSI (.svs file) given: coordinates, dimensions (optional), and output size(optional)
+        
+        Parameters
+        ----------
+        coordinates: tuple, list, np.ndarray
+            Top left coordinates of the subsection to be extracted given in pixels at the highest resolution of the file.
+        dim : int, float, tuple, list, np.ndarray
+            Dimensions of subsection to be extracted, given in the form of (width,height). If only one number is given
+            a square with sides of `dim` length is extracted.
+        output_size: int, float, tuple, list, np.ndarray
+            size of output, i.e. downsampling or upsampling to have these dimensions
+
+        Returns
+        -------
+        svs_np_array : np.ndarray
+            RGB array of WSI subsection in the format of M*N*3
+        """
+
+
 plt.imshow(svs_np_array)
